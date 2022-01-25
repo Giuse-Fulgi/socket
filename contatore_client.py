6c87fc5811cb5a8677835c169cdb2209a0ea9214
Giuse-Fulgi <giuseppe.fulginiti@alessandrinimainardi.edu.it>
2022-01-25 10:28:39 +0100

    contatore

---
+++ contatore_client.py
@@ -0,0 +1,18 @@
+import socket
+import json
+
+HOST="127.0.0.1"
+PORT=65435
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    #AF_INEF: tipo indirizzo/protocollo usato; la famiglia dell'indirizzo
+    #STREAM: TCP. Indica la connessione, per l'appunto
+    s.connect((HOST, PORT))
+    while True:
+        print("Inserisci KO per uscire ")
+        messaggio=input("Inserisci una frase ")
+        if messaggio=="KO":
+            break
+        s.sendall(messaggio.encode("UTF-8"))
+        data=s.recv(1024)
+        print("Risultato: ",data.decode())
---
+++ contatore_server.py
@@ -0,0 +1,29 @@
+import socket
+import json
+
+HOST='127.0.0.1'
+PORT=65435
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))#tupla: array non modificabile
+    s.listen()
+    print("[*] In ascolto su %s:%d "%(HOST, PORT))
+    #conversione del client
+    clientsocket, address=s.accept()#accetta la conversione
+    con=1
+    with clientsocket as cs:
+        print("Connessione da ", address)
+        while True:
+            data=cs.recv(1024)
+            if not data:
+                break
+            data=data.decode()
+            stringa= data
+            print("Stringa ricevuta"+stringa)
+            if stringa!="KO":
+                ris=stringa+""+str(con)
+                con+=1
+            ris=str(ris)
+            cs.sendall(ris.encode("UTF-8"))
+
+          
